e6076716a61fae1175f06fbc5783f2dd7dbe262b
Hunter <hunterthedev@gmail.com>
2018-10-25 23:34:29 -0400

    lines ready for separation

--- off2js.go
+++ off2js.go
@@ -21,6 +21,7 @@ func toInt(s string) int {
 	return i
 }
 
+
 func main() {
 	inPtr := flag.String("i", "foo.off", "the desired input file name")
 	outPtr := flag.String("o", "foo.js", "the desired output file name")
@@ -39,27 +40,38 @@ func main() {
 	scanner := bufio.NewScanner(input)
 	scanner.Scan()
 
+	// If this is an OFF file
 	if strings.ToUpper(scanner.Text()) == "OFF" {
-		scanner.Scan()
-		lineCounts := strings.Fields(scanner.Text())
-		numVertices := toInt(lineCounts[0])
-		numFaces := toInt(lineCounts[1])
-
+		var numVertices int
+		var numFaces int
+		gotCounts := false
+		verticesRead := false
 		i := 0
 		for scanner.Scan() {
-
-			i++
-			if i > numVertices {
-				break
+			// Do stuff if the line is not a comment
+			if !(string(scanner.Text()[0]) == "#"){
+				if !gotCounts {
+					nums := strings.Fields(scanner.Text())
+					numVertices = toInt(nums[0])
+					numFaces = toInt(nums[1])
+					gotCounts = true
+					fmt.Println(numVertices, numFaces)
+				} else if numVertices != 0 && numFaces != 0 {
+					if !verticesRead {
+						fmt.Println("Vertex: ", scanner.Text())
+						i++
+						if i == numVertices { verticesRead = true }
+					} else {
+						fmt.Println("Face: ", scanner.Text())
+					}
+				} else {
+					fmt.Println("Cannot define a model")
+					os.Exit(0)
+				}
 			}
 		}
-
-		i = 0
-		for scanner.Scan() {
-			i++
-			if i > numFaces {
-				break
-			}
-		}
+	} else {
+		fmt.Println("No OFF header found")
+		os.Exit(0)
 	}
 }
